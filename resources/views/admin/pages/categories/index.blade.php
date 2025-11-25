9aa1a3e5f96cc9281fcc6321f20e84adb3120508
qalesyaSN <b3byinjector@gmail.com>
2025-11-25 16:45:01 +0700

    Auto-commit: 2025-11-25 16:45:00

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -37,33 +37,23 @@
         </table>
     </div>
     <div class="col-md-6">
-        <table class="table">
-            <th>Title</th>
-            <th>Status </th>
-            <th>Action</th>
-            <tbody>
-                @forelse($categories as $category)
-                <tr>
-                    <td>{{ $category->name }}</td>
-                    <td><div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-      </div></td>
-                    <td>
-                        
-                        <form action="{{ route('admin.pages.categories.destroy', $category->id) }}" method="POST" style="display: inline-block" onsubmit="return confirm('Are you sure you want to delete it?');">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger"><i class="bi bi-trash"></i></button>
-                        </form>
-                    </td>
-                </tr>
-                @empty
-                <tr>
-                    <td colspan="7"><center><i class="text-center">Tidak ada data !</i></center></td>
-                </tr>
-                @endforelse
-            </tbody>
-        </table>
+        <div class="card card-body">
+        <form action="" method="">
+            <div class="form-group mb-3">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" name="name" placeholder="Special">
+            </div>
+            <div class="form-group mb-3">
+                <select name="" class="form-control">
+                    <option value="">Active</option>
+                    <option value="">Nonactive</option>
+                </select>
+            </div>
+            <div class="">
+                <button type="submit" class="btn btn-success">Add</button>
+            </div>
+        </form>
+    </div>
     </div>
     </div>
 </div>
