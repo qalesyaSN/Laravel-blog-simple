a334773ab6338bf529d70e06e7f7ace6b227aca7
qalesyaSN <b3byinjector@gmail.com>
2025-11-26 18:10:01 +0700

    Auto-commit: 2025-11-26 18:10:00

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::middleware(['auth'])
         
         // URL: /admin/posts/create
         // Route Name: admin.posts.create
-        Route::post('/posts/edit/{id}', [PostController::class, 'edit'])->name('pages.posts.edit');
+        Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('pages.posts.edit');
         
         // URL: /admin/posts/create
         // Route Name: admin.posts.create
