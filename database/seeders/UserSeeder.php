ed290e3678e66530332987cf520dff822a7bb183
qalesyaSN <b3byinjector@gmail.com>
2025-12-01 19:30:00 +0700

    Auto-commit: 2025-12-01 19:30:00

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash; // <--- Jangan lupa import ini buat password
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,26 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // 1. Akun ADMIN UTAMA (Punya Nunu)
         User::updateOrCreate(
-            ['email' => 'admin@gmail.com'], // Cek: Kalau email ini sudah ada, jangan buat baru
+            ['email' => 'admin@gmail.com'],
             [
-                'name'     => 'Admin Nunu',
-                'password' => Hash::make('password'), // Password default: password
-                // 'role'  => 'admin', (Kalau nanti ada kolom role, nyalakan ini)
-            ]
-        );
-
-        // 2. Akun USER BIASA (Buat ngetes login user lain)
-        User::updateOrCreate(
-            ['email' => 'user@gmail.com'],
-            [
-                'name'     => 'User Biasa',
+                'name'     => 'Admin',
                 'password' => Hash::make('password'),
             ]
         );
 
-        // 3. BONUS: Generate 10 User Random (Buat ngetes Pagination nanti)
         // User::factory(10)->create(); 
     }
 }
